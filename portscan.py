2422fb1186c79b33a1b21d0413ee4a1cf506be50
Rafael Ferreira <89147846+rafaelsousaferreira@users.noreply.github.com>
2021-08-22 10:31:56 -0300

    Update portscan.py
    Adicionado uma barra de progresso e um loop até a porta  65535 TCP. E mudado para python3

--- portscan.py
+++ portscan.py
@@ -1,26 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
-import socket
-import sys
+import socket,time,sys
+from tqdm import tqdm
+from termcolor import colored
 
-
-if len(sys.argv) <= 2 :
+if len(sys.argv) <= 1 :
 
 	print "Modo de uso: 192.168.100.1 80"
 
 else :
-	ip = sys.argv[1]
-	porta = int(sys.argv[2])
+	
+	for i in tqdm(range(0,65535)):
+                time.sleep(0.1)
+		ip = sys.argv[1]
+		porta = i
 
-	meusocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-	resp = 0
-	resp = meusocket.connect_ex((ip,porta))
+		meusocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+		resp = 0
+		resp = meusocket.connect_ex((ip,porta))
 
-if (resp == 0) :
+		if (resp == 0) :
 
-	print "[PORTA ABERTA]"
-
-else :
-
-	print "[PORTA FECHADA] "
+			print (colored("\n \n [PORTA ABERTA]-->",'red'),porta)
